c5ca3a5f7aad056fd9b2231d8a5b9a8050640687
Nishant77thakur <135938959+Nishant77thakur@users.noreply.github.com>
2026-06-22 13:02:26 +0530

    updated docker-compose and main.tf

--- infra(tf)/main.tf
+++ infra(tf)/main.tf
@@ -13,53 +13,69 @@ terraform {
     }
   }
 }
-
 provider "aws" {
   region = "ap-south-1"
 }
-resource "aws_security_group" "main-server-sg" {
-  name        = "main-server-sg"
-  description = "Security group for main server"
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+
+resource "aws_vpc" "k3s_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.k3s_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.k3s_vpc.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.k3s_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_security_group" "k3s_sg" {
+  vpc_id = aws_vpc.k3s_vpc.id
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port = 443
-    to_port   = 443
-    protocol  = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] 
   }
   egress {
-    from_port = 3306
-    to_port   = 3306
-    protocol  = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}  
-resource "aws_instance" "main_server" {
-  ami           = "ami-07a00cf47dbbc844c"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.main-server-sg.id]
-  subnet_id = "subnet-0ce9e8bded250e41b"
-  associate_public_ip_address = true
-  tags = {
-    Name = "Main-server"
-  }
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt update -y
-              sudo apt install nginx -y
-              sudo systemctl start nginx
-              sudo systemctl enable nginx
+}
 
-              EOF
+resource "aws_instance" "k3s_master" {
+  ami                    = "ami-01a00762f46d584a1" 
+  instance_type          = "t3.small"
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.k3s_sg.id]
+  key_name               = "ansible-keypair"
+  tags                   = { Name = "k3s-master" }
+}
+
+resource "aws_instance" "k3s_worker" {
+  ami                    = "ami-01a00762f46d584a1"
+  instance_type          = "t3.small"
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.k3s_sg.id]
+  key_name               = "ansible-keypair"
+  tags                   = { Name = "k3s-worker" }
 }
